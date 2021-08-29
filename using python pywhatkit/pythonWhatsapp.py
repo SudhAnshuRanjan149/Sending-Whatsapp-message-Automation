f4bd91ec0c7dafceb5927eaae2456330ece13214
Sudhanshu Ranjan <sudhanshuranjan149@gmail.com>
2021-08-29 17:31:05 +0530

    second commit

--- using python pywhatkit/pythonWhatsapp.py
+++ using python pywhatkit/pythonWhatsapp.py
@@ -1,11 +1,14 @@
+#first install pywhatkit by --> pip install pywhatkit
 # importing the module
 import pywhatkit
 
 # using Exception Handling to avoid unprecedented errors
 try:
-    # sending message to reciever
-    # using pywhatkit
-    pywhatkit.sendwhatmsg("+917987245525","Hello from Python PyWhatKit😜🤣",17, 9)
+    # sending message to reciever using pywhatkit
+
+    # pywhatkit.sendwhatmsg("write phone number with country code on which you wish to send message","write your message here--> Hello from Python PyWhatKit😜🤣",write here time in hour and minute-->hour, minute)
+    # for example
+    pywhatkit.sendwhatmsg("+9179********","Hello from Python PyWhatKit😜🤣",17, 9)
     print("Successfully Sent!")
 except:
 
